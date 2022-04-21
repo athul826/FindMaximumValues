c0bbc64db6c4149c83d0ce52f77a5e82b623d8c4
athul_tharol <athultharol1994@gmail.com>
2022-04-21 22:42:33 +0530

    maximum values programs added

---
+++ MaxtTestInteger.java
@@ -0,0 +1,35 @@
+package com.bridegelabz;
+
+import org.junit.jupiter.api.Test;
+
+import org.junit.jupiter.api.Assertions;
+
+public class MaxtTestInteger {
+
+	@Test
+    public void findMaxTC1() {
+    
+		Integer intValue1 = 800 , intValue2 = 557 , intValue3 = 500;    	
+    	FindMaximum find = new FindMaximum(intValue1, intValue2, intValue3);
+    	Integer maxIntValue = (Integer) find.testMaximum(); 
+		Assertions.assertEquals(800, maxIntValue);
+    }
+	
+	@Test
+    public void findMaxTC2() {
+    
+		Integer intValue1 = 800 , intValue2 = 850 , intValue3 = 500;    	
+    	FindMaximum find = new FindMaximum(intValue1, intValue2, intValue3);
+    	Integer maxIntValue = (Integer) find.testMaximum(); 
+		Assertions.assertEquals(850, maxIntValue);
+    }
+	
+	@Test
+    public void findMaxTC3() {
+    
+		Integer intValue1 = 800 , intValue2 = 557 , intValue3 = 930;    	
+    	FindMaximum find = new FindMaximum(intValue1, intValue2, intValue3);
+    	Integer maxIntValue = (Integer) find.testMaximum(); 
+		Assertions.assertEquals(930, maxIntValue);
+    }
+}
---
+++ StringMaxTest.java
@@ -0,0 +1,45 @@
+package com.bridegelabz;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+
+public class StringMaxTest {
+
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+    
+    @Test
+	public void findMaxTC1() {
+    	String stringValue1 = "Mango" , stringValue2 = "PineApple" , stringValue3 = "Orange";    	
+    	FindMaximum find = new FindMaximum(stringValue1, stringValue2, stringValue3);
+    	String maxStringValue = (String) find.testMaximum(); 
+		Assertions.assertEquals("PineApple", maxStringValue);
+	}
+    
+	@Test
+	public void findMaxTC2() {
+		
+		String stringValue1 = "Guava" , stringValue2 = "PineApple" , stringValue3 = "Orange";    	
+    	FindMaximum find = new FindMaximum(stringValue1, stringValue2, stringValue3);
+    	String maxStringValue = (String) find.testMaximum(); 
+		Assertions.assertEquals("PineApple", maxStringValue);
+		
+	}
+	
+	@Test
+	public void findMaxTC3() {
+		String stringValue1 = "Guava" , stringValue2 = "Apple" , stringValue3 = "Pineapple";    	
+    	FindMaximum find = new FindMaximum(stringValue1, stringValue2, stringValue3);
+    	String maxStringValue = (String) find.testMaximum(); 
+		Assertions.assertEquals("Pineapple", maxStringValue);
+	}
+}
